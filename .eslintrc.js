6eaa014184d263675fc7b06c3fb61de7d9cdf10b
Roy Surjano <rsurjano@outlook.com>
2020-02-29 17:01:07 -0500

    feat(tree): ✨ adding tree component

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+const base = require('@umijs/fabric/dist/eslint');
+
+module.exports = {
+  ...base,
+  rules: {
+    ...base.rules,
+    'react/sort-comp': 0,
+    'default-case': 0,
+    'eslint-comments/disable-enable-pair': 0,
+    'react/require-default-props': 0,
+    'react/no-unused-prop-types': 1,
+    'jsx-a11y/label-has-for': 0,
+    'jsx-a11y/label-has-associated-control': 0,
+  },
+};
